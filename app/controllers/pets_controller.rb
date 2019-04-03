4d6a44d17dc1830bd5d9b65d0e92412496eb76a2
github username <github email address>
2019-04-03 15:00:57 -0400

    done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,47 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'pets/index'
   end
 
-  get '/pets/new' do 
-    erb :'/pets/new'
+  get '/pets/new' do
+    @owners = Owner.all
+    erb :'pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(params[:pet])
+    if params[:owner][:name] != ""
+      @owner = Owner.create(params[:owner])
+      @pet.owner = @owner
+      @owner.save
+      @pet.save
+    end
+    redirect "/pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+  
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if params[:owner][:name] != ""
+      @owner = Owner.create(params[:owner])
+      @pet.owner = @owner
+      @pet.save
+    end
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @owner = Owner.find_by(@pet.owner_id)
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
-  end
+  
 end
